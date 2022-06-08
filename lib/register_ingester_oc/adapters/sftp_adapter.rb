d4d35578737fd91e2f3023c26347928d34d320fc
Josh Williams <josh@spacesnottabs.com>
2022-06-08 00:17:27 +0000

    Download by shelling out

--- lib/register_ingester_oc/adapters/sftp_adapter.rb
+++ lib/register_ingester_oc/adapters/sftp_adapter.rb
@@ -4,9 +4,11 @@ module RegisterIngesterOc
   module Adapters
     class SftpAdapter
       def download_file(host:, username:, password:, rem_path:, dst_path:)
-        Net::SFTP.start(host, username, password: password) do |sftp|
-          sftp.download!(rem_path, dst_path)
-        end
+        # Net::SFTP.start(host, username, password: password) do |sftp|
+        #  sftp.download!(rem_path, dst_path)
+        # end
+
+        `sftp #{username}@#{host}:#{rem_path} #{dst_path}`
       end
 
       def remote_file_stream(host:, username:, password:, rem_path:)
