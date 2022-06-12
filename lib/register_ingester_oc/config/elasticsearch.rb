2d0e4108dc77ece48b700deee87d0735fbd5175c
Josh Williams <josh@spacesnottabs.com>
2022-06-12 23:16:15 +0000

    WIP adding oc ingester using register_sources gem

---
+++ lib/register_ingester_oc/config/elasticsearch.rb
@@ -0,0 +1,12 @@
+require 'elasticsearch'
+require_relative 'settings'
+
+module RegisterIngesterOc
+  module Config
+    ELASTICSEARCH_CLIENT = Elasticsearch::Client.new(
+      host: "http://elastic:#{ENV['ELASTICSEARCH_PASSWORD']}@#{ENV['ELASTICSEARCH_HOST']}:#{ENV['ELASTICSEARCH_PORT']}",
+      transport_options: { ssl: { verify: false } },
+      log: false
+    )
+  end
+end
--- lib/register_ingester_oc/services/company_importer.rb
+++ lib/register_ingester_oc/services/company_importer.rb
@@ -1,17 +1,28 @@
+require 'tmpdir'
+require 'register_ingester_oc/config/adapters'
+require 'register_ingester_oc/services/company_reader'
+
 module RegisterIngesterOc
   module Services
     class CompanyImporter
       BATCH_SIZE = 100
 
-      def initialize(company_repository:, reader:, s3_adapter:)
+      def initialize(
+        company_repository:,
+        reader: CompanyReader.new,
+        s3_adapter: Config::Adapters::S3_ADAPTER
+      )
         @company_repository = company_repository
         @reader = reader
         @s3_adapter = s3_adapter
       end
 
       def import_from_s3(s3_bucket:, s3_path:, file_format: 'csv', zipped: true)
-        stream = s3_adapter.download_from_s3_to_memory(s3_bucket: s3_bucket, s3_path: s3_path)
-        import_from_stream(stream, file_format: file_format, zipped: zipped)
+        Dir.mktmpdir do |dir|
+          file_path = File.join(dir, "tmpfile")
+          s3_adapter.download_from_s3(s3_bucket: s3_bucket, s3_path: s3_path, local_path: file_path)
+          import_from_local_path(file_path, file_format: file_format, zipped: zipped)
+        end
       end
 
       def import_from_local_path(file_path, file_format: 'csv', zipped: true)
@@ -26,11 +37,12 @@ module RegisterIngesterOc
         reader.foreach(stream, file_format: file_format, zipped: zipped) do |record|
           batch_records << record
           next unless (batch_records.length >= BATCH_SIZE)
-          company_repository.store batch_records
+          # company_repository.store batch_records
+          print "STORING RECORDS: ", batch_records, "\n\n\n"
           batch_records = []
         end
         unless batch_records.empty?
-          company_repository.store batch_records
+          # company_repository.store batch_records
         end
       end
 
