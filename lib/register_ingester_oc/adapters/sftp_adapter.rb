bf96dab5d42c446fdf6a4e874d1192bae9a447d9
Josh Williams <josh@spacesnottabs.com>
2022-07-10 13:31:20 +0000

    Remove unused remote_file_stream

--- lib/register_ingester_oc/adapters/sftp_adapter.rb
+++ lib/register_ingester_oc/adapters/sftp_adapter.rb
@@ -1,22 +1,9 @@
-require 'net/sftp'
 
 module RegisterIngesterOc
   module Adapters
     class SftpAdapter
       def download_file(host:, username:, password:, rem_path:, dst_path:)
-        # Net::SFTP.start(host, username, password: password) do |sftp|
-        #  sftp.download!(rem_path, dst_path)
-        # end
-
         `sftp #{username}@#{host}:#{rem_path} #{dst_path}`
-      end
-
-      def remote_file_stream(host:, username:, password:, rem_path:)
-        Net::SFTP.start(host, username, password: password) do |sftp|
-          sftp.file.open(rem_path, "r") do |stream|
-            yield stream
-          end
-        end
       end
     end
   end
