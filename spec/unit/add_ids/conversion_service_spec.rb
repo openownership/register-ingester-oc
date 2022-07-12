8c2774bed8604a26f703e2db85f7149f175a242e
Josh Williams <josh@spacesnottabs.com>
2022-07-12 00:25:11 +0000

    Added tests for add_ids and alt_names

---
+++ spec/unit/add_ids/conversion_service_spec.rb
@@ -0,0 +1,57 @@
+require 'register_ingester_oc/add_ids/conversion_service'
+
+RSpec.describe RegisterIngesterOc::AddIds::ConversionService do
+  subject do
+    described_class.new(
+      athena_adapter: athena_adapter,
+      athena_database: athena_database,
+      s3_bucket: s3_bucket,
+      raw_table_name: raw_table_name,
+      processed_table_name: processed_table_name,
+      filtered_table_name: filtered_table_name
+    )
+  end
+
+  let(:athena_adapter) { double 'athena_adapter' }
+  let(:athena_database) { 'athena_database' }
+  let(:s3_bucket) { 's3_bucket' }
+  let(:raw_table_name) { 'raw_table_name' }
+  let(:processed_table_name) { 'processed_table_name' }
+  let(:filtered_table_name) { 'filtered_table_name' }
+
+  describe '#call' do
+    let(:month) { '2022_05' }
+    let(:jurisdiction_codes) { ['gb', 'dk'] }
+
+    it 'calls athena with correct queries' do
+      # Repair table query
+      repair_execution_id = double 'repair_execution_id'
+      repair_execution = double 'repair', query_execution_id: repair_execution_id
+      expect(athena_adapter).to receive(:start_query_execution).with(
+        query_string: "MSCK REPAIR TABLE raw_table_name\n",
+        result_configuration: { output_location: "s3://s3_bucket/athena_results" }
+      ).and_return repair_execution
+      expect(athena_adapter).to receive(:wait_for_query).with repair_execution_id
+
+      # Convert table query
+      convert_execution_id = double 'convert_execution_id'
+      convert_execution = double 'convert', query_execution_id: convert_execution_id
+      expect(athena_adapter).to receive(:start_query_execution).with(
+        query_string: "INSERT INTO processed_table_name\nSELECT * FROM raw_table_name\nWHERE mth = '2022_05';\n",
+        result_configuration: { output_location: "s3://s3_bucket/athena_results" }
+      ).and_return convert_execution
+      expect(athena_adapter).to receive(:wait_for_query).with convert_execution_id
+
+      # Inserts into filtered table
+      filter_execution_id = double 'filter_execution_id'
+      filter_execution = double 'filter', query_execution_id: filter_execution_id
+      expect(athena_adapter).to receive(:start_query_execution).with(
+        query_string: "INSERT INTO filtered_table_name\nSELECT\n  company_number,\n  uid,\n  identifier_system_code,\n  mth,\n  jurisdiction_code\nFROM processed_table_name\nWHERE mth = '2022_05' AND jurisdiction_code IN ('gb', 'dk');\n",
+        result_configuration: { output_location: "s3://s3_bucket/athena_results" }
+      ).and_return filter_execution
+      expect(athena_adapter).to receive(:wait_for_query).with filter_execution_id
+
+      subject.call month, jurisdiction_codes: jurisdiction_codes
+    end
+  end
+end
---
+++ spec/unit/add_ids/file_reader_spec.rb
@@ -0,0 +1,108 @@
+require 'register_ingester_oc/add_ids/file_reader'
+
+RSpec.describe RegisterIngesterOc::AddIds::FileReader do
+  subject { described_class.new(reader: reader, s3_adapter: s3_adapter, batch_size: batch_size) }
+
+  let(:reader) { double 'reader' }
+  let(:s3_adapter) { double 's3_adapter' }
+  let(:batch_size) { 2 }
+
+  describe '#import_from_s3' do
+    it 'downloads s3 file and yields batched rows' do
+      s3_bucket = double 's3_bucket'
+      s3_path = double 's3_path'
+      file_format = double 'file_format'
+      zipped = double 'zipped'
+
+      tmpdir = '/tmp/fake_tmp_dir'
+      file_path = File.join(tmpdir, "tmpfile")
+
+      expect(Dir).to receive(:mktmpdir).and_yield tmpdir
+      expect(s3_adapter).to receive(:download_from_s3).with(
+        s3_bucket: s3_bucket,
+        s3_path: s3_path,
+        local_path: file_path
+      )
+
+      stream = double 'stream'
+      expect(File).to receive(:open).with(file_path, 'r').and_yield stream
+
+      record1 = double 'record1'
+      record2 = double 'record2'
+      record3 = double 'record3'
+      expect(reader).to receive(:foreach).with(
+        stream,
+        file_format: file_format,
+        zipped: zipped
+      ).and_yield(record1).and_yield(record2).and_yield(record3)
+
+      # perform call
+      results = []
+      subject.import_from_s3(
+        s3_bucket: s3_bucket, s3_path: s3_path, file_format: file_format, zipped: zipped
+      ) do |batch_records|
+        results << batch_records
+      end
+
+      expect(results).to eq [[record1, record2], [record3]]
+    end
+  end
+
+  describe '#import_from_local_path' do
+    it 'streams file and yields batched rows' do
+      file_format = double 'file_format'
+      zipped = double 'zipped'
+      tmpdir = '/tmp/fake_tmp_dir'
+      file_path = File.join(tmpdir, "tmpfile")
+
+      stream = double 'stream'
+      expect(File).to receive(:open).with(file_path, 'r').and_yield stream
+
+      record1 = double 'record1'
+      record2 = double 'record2'
+      record3 = double 'record3'
+      expect(reader).to receive(:foreach).with(
+        stream,
+        file_format: file_format,
+        zipped: zipped
+      ).and_yield(record1).and_yield(record2).and_yield(record3)
+
+      # perform call
+      results = []
+      subject.import_from_local_path(
+        file_path, file_format: file_format, zipped: zipped
+      ) do |batch_records|
+        results << batch_records
+      end
+
+      expect(results).to eq [[record1, record2], [record3]]
+    end
+  end
+
+  describe '#import_from_stream' do
+    it 'processes streams and yields batched rows' do
+      file_format = double 'file_format'
+      zipped = double 'zipped'
+      stream = double 'stream'
+
+      record1 = double 'record1'
+      record2 = double 'record2'
+      record3 = double 'record3'
+      expect(reader).to receive(:foreach).with(
+        stream,
+        file_format: file_format,
+        zipped: zipped
+      ).and_yield(record1).and_yield(record2).and_yield(record3)
+
+      # perform call
+      results = []
+      subject.import_from_stream(
+        stream, file_format: file_format, zipped: zipped
+      ) do |batch_records|
+        results << batch_records
+      end
+
+      expect(results).to eq [[record1, record2], [record3]]
+    end
+  end
+end
