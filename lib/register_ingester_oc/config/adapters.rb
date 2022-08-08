bae1f0f6782b01e658c02b65bfdcd4fe1ef2538b
Josh Williams <josh@spacesnottabs.com>
2022-08-08 04:37:56 +0000

    Updated to use s3 adapter from register common

--- lib/register_ingester_oc/config/adapters.rb
+++ lib/register_ingester_oc/config/adapters.rb
@@ -1,12 +1,15 @@
+require 'register_common/adapters/s3_adapter'
+
 require 'register_ingester_oc/adapters/athena_adapter'
-require 'register_ingester_oc/adapters/s3_adapter'
 require 'register_ingester_oc/adapters/sftp_adapter'
+require 'register_ingester_oc/config/settings'
 
 module RegisterIngesterOc
   module Config
     module Adapters
+      S3_ADAPTER = RegisterCommon::Adapters::S3Adapter.new(credentials: AWS_CREDENTIALS)
+
       ATHENA_ADAPTER = RegisterIngesterOc::Adapters::AthenaAdapter.new
-      S3_ADAPTER = RegisterIngesterOc::Adapters::S3Adapter.new
       SFTP_ADAPTER = RegisterIngesterOc::Adapters::SftpAdapter.new
     end
   end
